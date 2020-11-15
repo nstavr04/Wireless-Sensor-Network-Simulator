5f9ed8b119d79c711cf3387815950b33fae0669a
Nikolas Stavrou <nstavr04@ucy.ac.cy>
2020-11-15 17:53:23 +0200

    Updated to sdk 15

--- src/Testing.java
+++ src/Testing.java
@@ -1,7 +1,7 @@
 public class Testing {
 
     public static void main(String[] args) {
-        System.out.println("Everything alright!");
+        System.out.println("Everything alrighty!");
 
     }
 }
