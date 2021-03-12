6b28a6bd837c619043eaa43cd4fe01cea9d058d9
NickTheGreek <60655570+nstavr04@users.noreply.github.com>
2021-03-12 20:14:15 +0200

    Update

--- src/cy/ac/ucy/cs/epl231/IDs1016662_1011050/homework3/Node.java
+++ src/cy/ac/ucy/cs/epl231/IDs1016662_1011050/homework3/Node.java
@@ -1,37 +1,36 @@
-package cy.ac.ucy.cs.epl231.IDs1016662_1011050.homework3;
 
 import java.awt.*;
 import java.util.LinkedList;
 
 // This class is using java's Point class. API:
 // https://docs.oracle.com/javase/7/docs/api/java/awt/Point.html
-public class Node_1011050_1016662 {
+public class Node {
 
     private final int ID;
     private Point coordinates;
     private int temperature;
 
-    private LinkedList<Node_1011050_1016662> neighbors;
+    private LinkedList<Node> neighbors;
 
 
-    public Node_1011050_1016662(int ID, Point coordinates, int temperature) {
+    public Node(int ID, Point coordinates, int temperature) {
         this.ID = ID;
         this.coordinates = new Point(coordinates);  // Copy constructor
         this.temperature = temperature;
-        neighbors = new LinkedList<Node_1011050_1016662>();
+        neighbors = new LinkedList<Node>();
     }
 
 
-    public Node_1011050_1016662(int ID, int coordinateX, int coordinateY, int temperature) {
+    public Node(int ID, int coordinateX, int coordinateY, int temperature) {
         this.ID = ID;
         coordinates = new Point(coordinateX, coordinateY);
         this.temperature = temperature;
-        neighbors = new LinkedList<Node_1011050_1016662>();
+        neighbors = new LinkedList<Node>();
     }
 
 
     // this method returns the distance d between two nodes
-    public double getDistance(Node_1011050_1016662 otherNode) {
+    public double getDistance(Node otherNode) {
         return (this.coordinates.distance(otherNode.coordinates));
     }
 
@@ -49,7 +48,7 @@ public class Node_1011050_1016662 {
     ****************** */
     // WE WILL NEVER NEED SOME OF THEM //
 
-    public LinkedList<Node_1011050_1016662> getNeighbors() { return neighbors; }
+    public LinkedList<Node> getNeighbors() { return neighbors; }
 
     public int getID() {
         return ID;
